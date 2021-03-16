c27482c20732f85232aa6b9fb821c5a05a38c41c
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-16 11:14:17 +0100

    js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+
+function random(max, min) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function lineToCenter(positionX, positionY, color) {
+  context.beginPath();
+  context.moveTo(positionX, positionY);
+  context.lineTo(canvas.width / 2, canvas.height / 2);
+  context.strokeStyle = color;
+  context.stroke();
+}
+
+for (var i = 0; i < 1000; i++) {
+  lineToCenter(random(canvas.width, 0), random(canvas.height, 0), 'teal')
+}
